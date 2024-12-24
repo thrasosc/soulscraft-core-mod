9933ddcfed04763d24f12823a403d365d3644487
Thrasos <100732009+thrasosc@users.noreply.github.com>
2024-12-24 18:17:36 +0100

    clean-up

--- common/src/main/java/net/pixeldreamstudios/soulscraft/item/armor/SoulsCraftArmorItem.java
+++ common/src/main/java/net/pixeldreamstudios/soulscraft/item/armor/SoulsCraftArmorItem.java
@@ -1,52 +1,21 @@
 package net.pixeldreamstudios.soulscraft.item.armor;
 
-import mod.azure.azurelib.common.api.client.renderer.GeoArmorRenderer;
 import mod.azure.azurelib.common.api.common.animatable.GeoItem;
-import mod.azure.azurelib.common.internal.client.RenderProvider;
 import mod.azure.azurelib.common.internal.common.util.AzureLibUtil;
 import mod.azure.azurelib.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.core.animation.AnimatableManager;
 import mod.azure.azurelib.core.animation.AnimationController;
 import mod.azure.azurelib.core.object.PlayState;
-import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.core.Holder;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.function.Consumer;
 
 public abstract class SoulsCraftArmorItem extends ArmorItem implements GeoItem {
     private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
-    private final GeoArmorRenderer armorRenderer;
 
-    public SoulsCraftArmorItem(Holder<ArmorMaterial> holder, Type type, GeoArmorRenderer armorRenderer) {
+    public SoulsCraftArmorItem(Holder<ArmorMaterial> holder, Type type) {
         super(holder, type, new Item.Properties());
-        this.armorRenderer = armorRenderer;
-    }
-
-    @Override
-    public void createRenderer(Consumer<RenderProvider> consumer) {
-        consumer.accept(new RenderProvider() {
-            /**
-             * Returns a custom humanoid armor model for the entity, based on the given equipment slot.
-             *
-             * @param livingEntity    The entity wearing the armor.
-             * @param itemStack       The item stack representing the armor.
-             * @param equipmentSlot   The equipment slot where the armor is equipped (e.g., head, chest).
-             * @param original        The original humanoid model.
-             * @return The humanoid model used for rendering the armor.
-             */
-            @Override
-            public @NotNull HumanoidModel<LivingEntity> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<LivingEntity> original) {
-                armorRenderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
-                return armorRenderer;
-            }
-        });
     }
 
     @Override
