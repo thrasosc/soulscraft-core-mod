81b3dfdaef2c9a7efaf37fe0ccdae7f6bec73be4
Thrasos <100732009+thrasosc@users.noreply.github.com>
2024-12-24 17:45:39 +0100

    remove `properties` parameter

--- common/src/main/java/net/pixeldreamstudios/soulscraft/item/armor/SoulsCraftArmorItem.java
+++ common/src/main/java/net/pixeldreamstudios/soulscraft/item/armor/SoulsCraftArmorItem.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,8 +24,8 @@ public abstract class SoulsCraftArmorItem extends ArmorItem implements GeoItem {
     private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
     private final GeoArmorRenderer armorRenderer;
 
-    public SoulsCraftArmorItem(Holder<ArmorMaterial> holder, Type type, Properties properties, GeoArmorRenderer armorRenderer) {
-        super(holder, type, properties);
+    public SoulsCraftArmorItem(Holder<ArmorMaterial> holder, Type type, GeoArmorRenderer armorRenderer) {
+        super(holder, type, new Item.Properties());
         this.armorRenderer = armorRenderer;
     }
 
