f4110789384b7a792d446b901ea72b102e6e9b80
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-01-18 18:08:16 +0100

    make SoulsCraftArmorAnimator extend AzItemAnimator instead of AzAnimator

--- common/src/main/java/net/pixeldreamstudios/soulscraft/item/armor/client/animator/SoulsCraftArmorAnimator.java
+++ common/src/main/java/net/pixeldreamstudios/soulscraft/item/armor/client/animator/SoulsCraftArmorAnimator.java
@@ -1,17 +1,18 @@
 package net.pixeldreamstudios.soulscraft.item.armor.client.animator;
 
-import mod.azure.azurelib.rewrite.animation.AzAnimator;
 import mod.azure.azurelib.rewrite.animation.controller.AzAnimationController;
 import mod.azure.azurelib.rewrite.animation.controller.AzAnimationControllerContainer;
+import mod.azure.azurelib.rewrite.animation.impl.AzItemAnimator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class SoulsCraftArmorAnimator extends AzAnimator {
+public abstract class SoulsCraftArmorAnimator extends AzItemAnimator {
     @Override
     public void registerControllers(AzAnimationControllerContainer animationControllerContainer) {
         animationControllerContainer.add(AzAnimationController.builder(this, "base_controller").build());
     }
 
     @Override
-    public abstract @NotNull ResourceLocation getAnimationLocation(Object animatable);
+    public abstract @NotNull ResourceLocation getAnimationLocation(ItemStack animatable);
 }
